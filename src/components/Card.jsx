424128fc5bb3d2fc742793d622931151b1c1974b
niv <nivroz93@gmail.com>
2021-07-02 15:39:57 +0300

    changed profileLow

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components/';
 // import Logo from '../Navbar/Logo';
-import ProfileImg from '../images/OUTSIDE-NUTEK.png'
+import ProfileImg from '../images/profileLow.jpg'
 
 const Card = () => {
     return (
