4da076e29a6f240f24d20ea2668ddd8d177e975a
niv <nivroz93@gmail.com>
2021-07-20 15:18:44 +0300

    fixed nesletter googleSheets

--- src/App.js
+++ src/App.js
@@ -1,70 +1,75 @@
-import React , {useState, useEffect }from 'react';
-import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { About } from './components/About'
-import { Music } from './components/Music';
-import { Contact } from './components/Contact';
-import  { Navigator }  from './Navbar/Nav';
-import { HomePage } from './components/HomePage';
-import { Footer } from './components/Footer';
-import Loader  from './components/Loader.js'
-import { Helmet } from 'react-helmet';
-import ProfileImg from './images/profileLow.jpg';
-
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { About } from "./components/About";
+import { Music } from "./components/Music";
+import { Contact } from "./components/Contact";
+import { Navigator } from "./Navbar/Nav";
+import { HomePage } from "./components/HomePage";
+import { Footer } from "./components/Footer";
+import Loader from "./components/Loader.js";
+import { Helmet } from "react-helmet";
+import ProfileImg from "./images/profileLow.jpg";
 
 function App() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
-    setTimeout(() =>{
+    setTimeout(() => {
       setLoading(false);
-    },1000 )
-  },[])
+    }, 1000);
+  }, []);
   return (
     <>
-        <Helmet>
-          <title>Guerrilla - Music</title>
-          <meta name="description" content="Guerrilla is an emerging duo formed
-          by Niv Rozanowich and Tomer Segev." />
-          <meta name="keywords" content="Guerrilla, Music, Psytrance, Genetics, Nutek" />
-          <meta name="og:title" content="Guerrilla - Music" />
-          <meta name="og:description" content="Guerrilla is an emerging duo formed
-          by Niv Rozanowich and Tomer Segev." />
-          <meta name="og:image" content="https://guerrillatrance.com/static/media/profileLow.a1c15d6a.jpg" />
-          <meta name="fb:app_id" content="1337322356737912" />
-
-        </Helmet>
-    {
-      loading ? (
+      <Helmet>
+        <title>Guerrilla - Music</title>
+        <meta
+          name="description"
+          content="Guerrilla is an emerging duo formed
+          by Niv Rozanowich and Tomer Segev."
+        />
+        <meta
+          name="keywords"
+          content="Guerrilla, Music, Psytrance, Genetics, Nutek"
+        />
+        <meta name="og:title" content="Guerrilla - Music" />
+        <meta
+          name="og:description"
+          content="Guerrilla is an emerging duo formed
+          by Niv Rozanowich and Tomer Segev."
+        />
+        <meta
+          name="og:image"
+          content="https://guerrillatrance.com/static/media/profileLow.a1c15d6a.jpg"
+        />
+        <meta name="fb:app_id" content="1337322356737912" />
+      </Helmet>
+      {loading ? (
         <Loader />
-        ) : (
-      <Router>
-        <Navigator />      
-        <Switch>
-        <Route exact path="/">
-            {/* <HomePage /> */}<About />
+      ) : (
+        <Router>
+          <Navigator />
+          <Switch>
+            <Route exact path="/">
+              {/* <HomePage /> */}
+              <About />
             </Route>
             <Route path="/music">
-            <Music />
+              <Music />
             </Route>
             <Route path="/contact">
-            <Contact />
-            </Route> 
+              <Contact />
+            </Route>
             {/* <Route path="/about">
             
             </Route>
             */}
-        </Switch>
-        <Footer />
-      </Router>
-      )
-    }
+          </Switch>
+          <Footer />
+        </Router>
+      )}
     </>
- 
   );
 }
 
 export default App;
-
-
-
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,101 +1,138 @@
-import React, { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import styled from 'styled-components/';
-import '../styles/signup.css';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import styled from "styled-components/";
+import "../styles/signup.css";
+import ThankYou from "./ThankYou";
 
+const SignUp = () => {
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+  });
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const { name, email } = data;
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
+  const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-const SignUp = ({onSubmit}) => {
-    const email_regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const { register, handleSubmit, formState: {errors} } = useForm();
-    
-    const [ data, setData ] = useState({
-        email:""
-    })
-    const { email } = data;
-    
-    const handleChange = (e) =>{
-        setData({...data, email: e.target.value });
-        console.log(e)
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    console.log({ e });
+    setIsSubmitted(true);
+    try {
+      const response = await fetch(
+        "https://v1.nocodeapi.com/nivo/google_sheets/rpRmEOzQUiakvjuT?tabId=Sheet1",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([
+            [e.name, e.email, new Date().toLocaleString()],
+          ]),
+        }
+      );
+      await response.json();
+      setData({ ...data, name: "", email: "" });
+    } catch (error) {
+      console.log(error);
     }
-    const onSubmit2= async (e)=>{
-        
-        onSubmit()
-        try{   
-            const response = await fetch(
-                'https://v1.nocodeapi.com/nivo/google_sheets/rpRmEOzQUiakvjuT?tabId=Sheet1', {
-                method: 'POST',
-                headers: { 
-                    'Content-Type': 'application/json'
+  };
+  return (
+    <Main>
+      <Newsletter>
+        {!isSubmitted ? (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <h1>Sign in to our newsletter</h1>
+            <Input
+              placeholder="Your name"
+              style={{
+                fontWeight: "italic",
+                color: "#000",
+              }}
+              {...register("name", {
+                required: true,
+                minLength: { value: 2, message: "Please enter a valid name" },
+                id: { name },
+                pattern: {},
+                onChange: { handleChange },
+              })}
+            />
+
+            {errors.name && <span role="alert">{errors.name.message}</span>}
+
+            <Input
+              placeholder="Your email"
+              style={{
+                fontWeight: "italic",
+                color: "#000",
+              }}
+              {...register("email", {
+                required: true,
+                minLength: {
+                  value: 8,
+                  message: "Please enter a valid email",
                 },
-                body: JSON.stringify([
-                    [email, new Date().toLocaleString()],
-                ])
-            }
-        )
-        await response.json();
-        setData({...data, email:''});
+                id: { email },
+                pattern: {
+                  value: email_regex,
+                  message: "Please enter a valid email",
+                },
+                onChange: { handleChange },
+              })}
+            />
 
-            }catch(error){
-            console.log(error)
-        }
-    }
-return (
-<Main>
-    <form noValidate onSubmit={handleSubmit(onSubmit2)}>
-        <h1>Sign in to our newsletter</h1>
-    <Input placeholder="Your email" style={{
-            fontWeight: "italic",
-            color: "#000"
-          }}
-     {...register('email', { 
-        required: true ,
-        minLength: { value:8, message: 'Please enter a valid email'},
-        id: { email } ,
-        pattern:{
-            value: email_regex, 
-            message: 'Please enter a valid email'
-        },
-        onClick:{handleChange}
-        }
-        )} 
-    />
-    {errors.email && <span role="alert">{errors.email.message}</span>}          
-    <Button type="submit" />
-    </form>
-</Main>
-)
-}
+            {errors.email && <span role="alert">{errors.email.message}</span>}
+            <Button type="submit" />
+          </form>
+        ) : (
+          <ThankYou />
+        )}
+      </Newsletter>
+    </Main>
+  );
+};
 
-export default SignUp
+export default SignUp;
 
+const Newsletter = styled.div`
+  display: flex;
+  flex: 1;
+`;
 const Main = styled.div`
-    display:flex;
-    position:relative;
-    width:80vw;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  position: relative;
+  width: 80vw;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 const Button = styled.input`
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    font-size:large;
-    border-radius: 10px;
-    color:black;
-    z-index:11;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: large;
+  border-radius: 10px;
+  color: black;
+  z-index: 11;
 `;
 const Input = styled.input`
-    font-size:large;
-    border-radius: 10px;
-    color:white;
-    width:80%;
-    height:20px;
-    z-index:11;
-    position:relative;
+  font-size: large;
+  border-radius: 10px;
+  color: white;
+  width: 80%;
+  height: 20px;
+  z-index: 11;
+  position: relative;
 
-    @media only screen and ( max-width: 1200px ){
-        width: 80% !important;
-        
-    }
+  @media only screen and (max-width: 1200px) {
+    width: 80% !important;
+  }
 `;
