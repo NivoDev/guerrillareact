9a16209e9625270c28f3444c6c49a70702d4973f
Nivo <nivo@Nivos-MacBook-Pro-2.local>
2025-06-04 22:37:50 +0300

    fix: add essential Open Graph tags to index.html for social media scrapers

--- src/App.js
+++ src/App.js
@@ -25,24 +25,8 @@ function App() {
           <title>Guerrilla - Progressive Psytrance Music</title>
           <meta name="description" content="Guerrilla is an emerging psytrance duo delivering powerful progressive beats. Experience our unique sound combining cultural influences with massive kick & bass sections." />
           <meta name="keywords" content="Guerrilla, Psytrance, Progressive Psytrance, Electronic Music, Nutek Records, Dance Music" />
-          <meta name="facebook-domain-verification" content="pcotu6lkgi9j7drra34gmyat9strh0" />
           
-          {/* Open Graph / Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://guerrillatrance.com/" />
-          <meta property="og:title" content="Guerrilla - Progressive Psytrance Music" />
-          <meta property="og:description" content="Guerrilla is an emerging psytrance duo delivering powerful progressive beats. Experience our unique sound combining cultural influences with massive kick & bass sections." />
-          <meta property="og:image" content="https://guerrillatrance.com/GuerrillaProfile2025.jpg" />
-          <meta property="fb:app_id" content="1337322356737912" />
-          
-          {/* Twitter */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:url" content="https://guerrillatrance.com/" />
-          <meta name="twitter:title" content="Guerrilla - Progressive Psytrance Music" />
-          <meta name="twitter:description" content="Guerrilla is an emerging psytrance duo delivering powerful progressive beats. Experience our unique sound combining cultural influences with massive kick & bass sections." />
-          <meta name="twitter:image" content="https://guerrillatrance.com/GuerrillaProfile2025.jpg" />
-          
-          {/* Additional SEO tags */}
+          {/* Additional SEO tags for browsers */}
           <meta name="robots" content="index, follow" />
           <meta name="language" content="English" />
           <meta name="revisit-after" content="3 days" />
